6dcb348844053e5c846039484b4c406a08c3bd5c
alba <arga.alba@gmail.com>
2021-10-25 16:36:13 -0300

    commit

--- pages/components/NavBar.js
+++ pages/components/NavBar.js
@@ -5,17 +5,17 @@ function NavBar() {
     <nav className="flex bg-red-100 sticky top-0 z-50  md:bg-contain w-screen p-4 text-1xl md:text-1xl  items-center justify-center md:space-x-28 space-x-6 md:text-center ... ">
       <Link href="/components/About">
         <button className=" uppercase py-2 px-4 bg-red-50 text-red-200  font-semibold rounded-lg shadow-md hover:bg-red-50 focus:outline-none focus:ring-2 focus:bg-red-50 ring-transparent">
-          <a>About</a>
+          About
         </button>
       </Link>
       <Link href="/components/Projects">
         <button className=" uppercase  py-2 px-4 bg-red-50 text-red-200 font-semibold rounded-lg shadow-md hover:bg-red-50 focus:outline-none focus:ring-2 focus:bg-red-50 ring-transparent">
-          <a> Projects </a>
+          Projects
         </button>
       </Link>
       <Link href="/components/Footer">
         <button className=" uppercase  py-2 px-4 bg-red-50 text-red-200 font-semibold rounded-lg shadow-md hover:bg-red-50 focus:outline-none focus:ring-2 focus:bg-red-50 ring-transparent">
-          <a>Contact</a>
+          Contact
         </button>
       </Link>
     </nav>
